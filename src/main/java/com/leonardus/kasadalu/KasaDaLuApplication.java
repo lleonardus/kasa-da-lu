c6487d284b75e6af845744bfb2931bfc033db2ad
leonardus <leonardus.wizard@gmail.com>
2022-12-21 16:24:50 -0300

    first commit

---
+++ src/main/java/com/leonardus/kasadalu/KasaDaLuApplication.java
@@ -0,0 +1,13 @@
+package com.leonardus.kasadalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasaDaLuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasaDaLuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardus/kasadalu/KasaDaLuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardus.kasadalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasaDaLuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
