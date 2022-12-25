5ac1f6c0856e04253414c0386d3a0812850e9c97
leonardus <leonardus.wizard@gmail.com>
2022-12-25 00:59:52 -0300

    Adicionando dados para povoar a API

---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+-- French Toasts
+
+INSERT INTO french_toasts(flavor, price) VALUES ('Tradicional', 4);
+INSERT INTO french_toasts(flavor, price) VALUES ('Chocolate', 5);
+INSERT INTO french_toasts(flavor, price) VALUES ('Doce de Leite', 5);
+INSERT INTO french_toasts(flavor, price) VALUES ('Nutella', 7);
+
+-- Clients
+
+INSERT INTO clients(name, lastname, phone) VALUES ('Leonardo', 'Sousa', '4002-8922');
+INSERT INTO clients(name, lastname, phone) VALUES ('Carlos', 'Ferreira', '8922-4002');
+INSERT INTO clients(name, lastname, phone) VALUES ('André', 'Santos', '4008-7922');
+INSERT INTO clients(name, lastname, phone) VALUES ('Márcia', 'Alves', '4052-8922');
+INSERT INTO clients(name, lastname, phone) VALUES ('Bianca', 'Fernandes', '4002-8522');
+
+-- Orders
+
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (1, 1, 3, '2022-12-15');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (1, 3, 1, '2022-11-27');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (1, 2, 2, '2022-10-3');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (2, 4, 4, '2022-11-8');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (2, 1, 3, '2022-12-15');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (3, 3, 1, '2022-11-27');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (3, 2, 2, '2022-10-3');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (4, 4, 4, '2022-11-8');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (4, 1, 3, '2022-12-15');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (5, 3, 1, '2022-11-27');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (5, 2, 2, '2022-10-3');
+INSERT INTO orders(client_id, french_toast_id, quantity, ordered_at) VALUES (5, 4, 4, '2022-11-8');
