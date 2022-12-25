f30fed0ea5d0940de115b852cc2d955f92dc7ec8
leonardus <leonardus.wizard@gmail.com>
2022-12-25 01:00:48 -0300

    Adicionando ModelMapper ao projeto

---
+++ src/main/java/com/leonardus/kasadalu/config/ModelMapperConfig.java
@@ -0,0 +1,11 @@
+package com.leonardus.kasadalu.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    ModelMapper mapper(){return new ModelMapper();}
+}
